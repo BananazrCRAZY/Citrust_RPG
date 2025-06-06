b239fd714f4eb46e3bc52448518f06dcb49dd69f
BananazrCRAZY <bananamandoge@gmail.com>
2025-06-05 18:33:34 -0700

    need to checkout

--- tests/BossTest.cpp
+++ tests/BossTest.cpp
@@ -2,6 +2,13 @@
 #include "Apple.h"
 #include "Pear.h"
 #include "Strawberry.h"
+#include "Grape.h"
+#include "Dekopon.h"
+#include "MangoGreen.h"
+#include "MangoRed.h"
+#include "Pineapple.h"
+#include "Durian.h"
+#include "Watermelon.h"
 
 TEST(BossTests, constructorFail) {
 
