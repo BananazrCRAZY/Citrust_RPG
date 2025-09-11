a671d92dd2367f9e98ca891fe299b35e0e8fccf4
BananazrCRAZY <76630376+BananazrCRAZY@users.noreply.github.com>
2025-09-10 20:37:57 -0700

    v0.3

--- include/bossHeaders/Dekopon.h
+++ include/bossHeaders/Dekopon.h
@@ -8,14 +8,34 @@ class Dekopon : public Boss {
         Dekopon(const string& main, const string& item, int required, const string& proxy) : Boss(main, item, required, proxy) {
             name = "Dekopon";
             level = 50;
+            maxHp->addBase(2000);
+            hp += 2000;
         }
         string specialAttack(Fruit* target) {
+            string returnThis = "";
+            int physDmg = attack->getTotal();
+            int artsDmg = arts->getTotal();
+            if (checkIfCrit()) {
+                physDmg *= (critDmg->getTotal()/100.0 + 1);
+                artsDmg *= (critDmg->getTotal()/100.0 + 1);
+                returnThis += "CRIT!\n";
+            }
+            physDmg -= target->getDefense();
+            if (physDmg <= 0) physDmg = 0;
+            artsDmg *= (1.0 - (target->getRes()/100.0));
+            if (artsDmg <= 0) artsDmg = 0;
+            int damageDealt = physDmg + artsDmg;
+            if (damageDealt <= 0) returnThis += name + ": Dealt 0 damage.\n";
+            else {
+                target->setHp(-1 * damageDealt);
+                returnThis += name + ": Dealt " + std::to_string(damageDealt) + " damage.\n";
+            }
             int amount = target->getMaxHp() * .2;
             setHp(amount);
             if (target->getHp() <= amount) target->setHp(-1 * (target->getHp() - 1));
             else target->setHp(-1 * amount);
             rechargeCount -= 2;
-            return name + ": Absorbed 20% of " + target->getName() + "\'s HP.";
+            return returnThis + name + ": Absorbed 20% of " + target->getName() + "\'s HP.";
         }
 
         string bossAbility() {
